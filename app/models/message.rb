4ebd16856e041e7c26213d5e252ac6e9f69b603d
nikita <nikita.afanasev2020@gmail.com>
2018-05-12 18:50:11 +0500

    implements socket fot chat

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :user
-
+  after_create_commit { MessageBroadcastJob.perform_now self }
   def as_json(options = nil)
     super({ only: [:id, :message_body,:message_type,:conversation_id, :user_id]}.merge(options || {}))
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  mount ActionCable.server => '/cable'
+
   resources :companies
   devise_for :admin_users, ActiveAdmin::Devise.config
 
